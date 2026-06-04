1280ed475fd2e5d00b14916de879ea462d205899
dannacarlos <danna.charlesuni@gmail.com>
2026-06-03 22:12:52 -0300

    Update Footer.jsx

---
+++ src/src/src/services/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <p>&copy; 2026 - APU - UNJU - Programación Visual - Grupo 2</p>
+    </footer>
+  );
+}
